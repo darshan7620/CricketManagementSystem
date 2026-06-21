a65e8c50786ca4fbee190f7b59740db74cceb340
Asus <Asus@DESKTOP-HAKMV4I>
2026-06-21 19:10:50 +0530

    IPL project creation

---
+++ IPL_Team-Config_Server/src/main/java/com/nt/IplTeamConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplTeamConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplTeamConfigServerApplication.class, args);
+	}
+
+}
---
+++ IPL_Team/src/main/java/com/nt/IplTeamApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplTeamApplication.class, args);
+	}
+
+}
