61213d064a62a347670f151641b5326d4babdf49
Asus <Asus@DESKTOP-HAKMV4I>
2026-06-21 19:46:20 +0530

    IPL API Gateway

---
+++ IPL_Team-API_Gateway/src/main/java/com/nt/IplTeamApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplTeamApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplTeamApiGatewayApplication.class, args);
+	}
+
+}
